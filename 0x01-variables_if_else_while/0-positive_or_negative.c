3de783d5a70c3139e04df9caeb55d4637ca09bf7
Samuel Adetoyinbo <adetoyinbosam99@gmail.com>
2022-10-14 14:22:13 -0700

    Change coding style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,12 +14,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n < 0) {
-		printf("%d\n is negative", n);
-	} else if (n > 0) {
-		printf("%d\n is positive", n);
-	} else {
-		printf("%d\n is zero", n);
+	if (n < 0)
+	{
+		printf("%d is negative", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive", n);
+	}
+	else
+	{
+		printf("%d is zero", n);
 	}
 	return (0);
 }
