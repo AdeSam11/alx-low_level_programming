eb309876022c68c35ffd9263468052574c6298d9
Samuel Adetoyinbo <adetoyinbosam99@gmail.com>
2022-10-14 16:44:11 -0700

    Modify code

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int one;
+	int ten;
+	int o;
+	int t
+
+	for (ten = '0'; ten <= '9'; ten++)
+	{
+		for (ten = (one = '0'; one <= '9'; one++)
+		{
+			for (t = ten; t <= '9'; t++)
+			{
+				for (o = (one + 1); o <= '9'; o++)
+				{
+					putchar(ten);
+					putchar(one);
+					putchar(' ');
+					putchar(t)
+					putchar(o)
+
+					if (!((ten == '9' && one == '8') &&
+						(t == '9' && o == '9')))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				o = '0';
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
