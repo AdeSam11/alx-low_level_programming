bbb6ee3a16950f3f44f54092e00f662b8a610785
Samuel Adetoyinbo <adetoyinbosam99@gmail.com>
2022-10-16 10:16:03 -0700

    Refactor code

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -28,9 +28,7 @@ int main(void)
 						putchar(t);
 						putchar(o);
 
-						if (!((ten == '9' && one == '8')
-							&&
-							(t == '9' && o == '9')))
+						if (!((ten == '9' && one == '8') && (t == '9' && o == '9')))
 						{
 							putchar(',');
 							putchar(' ');
