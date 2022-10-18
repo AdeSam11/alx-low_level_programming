ae0ddb06a0d2781247b5c7662260d6a4d1edb5ab
Samuel Adetoyinbo <adetoyinbosam99@gmail.com>
2022-10-18 12:52:33 -0700

    Change code style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,16 +2,16 @@
 #include "main.h"
 
 /**
- *  main- entry point
- *  description- prints the word _putchar, followed by a new line
- *  return: always 0 (success)
+ * main- entry point
+ * description- prints the word _putchar, followed by a new line
+ * Return: 0 (success)
  */
 int main(void)
 {
 	char word[8] = "_putchar";
 	int i;
 
-	for (i=0; i<8; i++)
+	for (i = 0; i < 8; i++)
 		_putchar(word[i]);
 
 	_putchar('\n');
