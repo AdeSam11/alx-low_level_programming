0c6993d99e441105c0cd7064f3ccfb73d08ee7be
Samuel Adetoyinbo <adetoyinbosam99@gmail.com>
2022-10-18 15:31:53 -0700

    Get the sum of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - check description
+ * Description: a function that adds two integers and returns the result
+ * @x: one of the input integers
+ * @y: one of the input integers
+ * Return: sum of x and y
+ */
+int add(int x, int y)
+{
+	sum = x + y;
+	return (sum);
+}
