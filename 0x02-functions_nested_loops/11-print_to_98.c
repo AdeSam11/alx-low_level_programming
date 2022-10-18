a5c08893515970ae9027aeea74b21d8361c33b75
Samuel Adetoyinbo <adetoyinbosam99@gmail.com>
2022-10-18 15:52:04 -0700

    Modify code

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,11 +12,14 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		printf("%d", n);
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
 
-		if (n == 98)
-			continue;
-		printf(", ");
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
 	}
 	else
 	{
