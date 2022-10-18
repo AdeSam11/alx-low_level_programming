cdf74c4be333f9fd8dce1389dccba76ee75d16c7
Samuel Adetoyinbo <adetoyinbosam99@gmail.com>
2022-10-18 14:38:42 -0700

    Change code style

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,6 +3,7 @@
 /**
  * _abs - see description
  * Description: a function that computes the absolute value of an integer
+ * @x: the input integer
  * Return: absolute value of the integer
  */
 int _abs(int x)
