29e593e07266e82bd6c1d2cfc12d06d0033469eb
Samuel Adetoyinbo <adetoyinbosam99@gmail.com>
2022-10-18 12:45:00 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ *  main- entry point
+ *  description- prints the word _putchar, followed by a new line
+ *  return: always 0 (success)
+ */
+int main(void)
+{
+	char word[8] = "_putchar";
+	int i;
+
+	for (i=0; i<8; i++)
+		_putchar(word[i]);
+
+	_putchar('\n');
+
+	return (0);
+}
