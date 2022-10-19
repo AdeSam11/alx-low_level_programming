3880f6f179c7aa0948b0f43614dc3fed03f34a94
Samuel Adetoyinbo <adetoyinbosam99@gmail.com>
2022-10-18 17:11:38 -0700

    Modify code

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,6 +6,8 @@
  * @n: the input number
  * Return: Nothing
  */
+void format_func(int n);
+
 void print_times_table(int n)
 {
 	int i, j;
@@ -16,9 +18,7 @@ void print_times_table(int n)
 		{
 			_putchar('0');
 			for (j = 1; j <= n; j++)
-			{
-				void format_func(i * j);
-			}
+				format_func(i * j);
 			_putchar('\n');
 		}
 	}
