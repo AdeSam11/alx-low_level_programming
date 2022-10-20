35c0140790652d4c1e7a088dc2cb8f10c2e4d047
Samuel Adetoyinbo <adetoyinbosam99@gmail.com>
2022-10-20 15:20:55 -0700

    Add putchar

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * mul: A function that multiplies two integers.
+ * mul - A function that multiplies two integers
  * @a: An input integer
  * @b: An input integer
- * Return: the multiple of a and b
+ * Return: Always 0
  */
 int mul(int a, int b)
 {
