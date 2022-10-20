743be0d680e0ada330214a78ab1aa4fafc900cc2
Samuel Adetoyinbo <adetoyinbosam99@gmail.com>
2022-10-20 15:57:47 -0700

    Modify code

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,10 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j);
+			if (j > 9)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
 	}
